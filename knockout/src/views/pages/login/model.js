d5a73b8431c29319c195a9ec1ba4c7b27d0badfb
Nikita <johndoe@example.com>
2017-04-07 18:34:47 +0300

    #Knockout - Simple validation on login.html

--- knockout/src/views/pages/login/model.js
+++ knockout/src/views/pages/login/model.js
@@ -38,9 +38,6 @@ setTimeout(function(){
                     slidesToScroll: 1
                 }
             }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
         ]
     });
 }, 0);
@@ -61,8 +58,61 @@ export default class HomepageVM{
 
     }
 
-    formValidate(group){
+    formValidate(event){
+        let form = $(event.target),
+            elements = form.find('input[required], select[required]'),
+            errorWrapper = $('.form__validation-state'),
+            errorText = errorWrapper.find('span'),
+            errorType = errorWrapper.find('strong'),
+            text = /^[a-z0-9]+$/i;
 
+
+        elements.removeClass('error');
+        elements.removeClass('success');
+
+        errorWrapper.removeClass('error');
+        errorWrapper.removeClass('success');
+
+        elements.each(function(index, el){
+            let type = $(el).attr('type'),
+                element = $(el),
+                val = $(el).val();
+
+            if(type === 'text' || type === 'password'){
+
+                if ( val.length > 0 && val != undefined && val != null){
+                    element.addClass('success');
+                } else {
+                    element.addClass('error');
+                }
+            }
+        });
+
+        if(this.setErrorMessage(elements)){
+            errorWrapper.addClass('error');
+            errorType.text('Error: ');
+            errorText.text('Not valid.');
+        } else {
+            errorWrapper.addClass('success');
+            errorType.text('Success. ');
+            errorText.text('');
+        }
+
+    }
+
+    setErrorMessage(elements) {
+        let errorCounter = 0;
+        if(elements.length > 0){
+            elements.each(function(i, el){
+               if( $(el).hasClass('error') ){
+                   errorCounter++;
+               }
+            });
+        }
+        if(errorCounter){
+            return true;
+        }
+        return false;
     }
 
     handleSubmit(data, event){
@@ -74,7 +124,8 @@ export default class HomepageVM{
             remember: this.loginModel().remember()
         };
 
-        console.dir(event);
+        this.formValidate(event);
+
 
         let jsonData = ko.toJSON(signInData);
 
@@ -87,6 +138,8 @@ export default class HomepageVM{
         let accountData = {
             username: this.accountModel().username()
         };
+
+        this.formValidate(event);
 
         let jsonData = ko.toJSON(accountData);
 
