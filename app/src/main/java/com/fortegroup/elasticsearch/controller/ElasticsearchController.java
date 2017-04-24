ced1820bca62a7f00796dc36e83d95415274609c
Eugene Pankov <panashka91@gmail.com>
2017-04-24 14:21:18 +0300

    Merge branch 'dev' of https://bitbucket.org/MarthusMarthus/electrohtml into dev
    # Conflicts:
    #	app/.idea/artifacts/shop_war_exploded.xml
    #	app/.idea/workspace.xml
    #	app/pom.xml
    #	app/shop.iml
    #	app/src/main/java/com/fortegroup/config/DatabaseConfig.java
    #	app/src/main/java/com/fortegroup/dao/implementation/UserDaoImpl.java
    #	app/src/main/java/com/fortegroup/security/controller/AuthenticationController.java
    #	app/src/main/java/com/fortegroup/security/filter/AuthenticationTokenFilter.java
    #	app/src/main/java/com/fortegroup/security/model/Message.java
    #	app/src/main/java/com/fortegroup/security/utill/MessageFactory.java
    #	app/src/main/java/com/fortegroup/service/UserServiceImpl.java
    #	app/src/main/webapp/assets/js/app.js
    #	app/src/main/webapp/assets/js/vendor.js
    #	app/target/ROOT.war
    #	app/target/classes/com/fortegroup/config/DatabaseConfig.class
    #	app/target/classes/com/fortegroup/dao/implementation/UserDaoImpl.class
    #	app/target/classes/com/fortegroup/security/controller/AuthenticationController.class
    #	app/target/classes/com/fortegroup/security/filter/AuthenticationTokenFilter.class
    #	app/target/classes/com/fortegroup/security/model/Message.class
    #	app/target/classes/com/fortegroup/security/utill/MessageFactory.class
    #	app/target/classes/com/fortegroup/service/UserServiceImpl.class
    #	app/target/maven-archiver/pom.properties
    #	app/target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst
    #	app/target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst
    #	app/target/shop-1.0-SNAPSHOT/WEB-INF/classes/com/fortegroup/config/DatabaseConfig.class
    #	app/target/shop-1.0-SNAPSHOT/WEB-INF/classes/com/fortegroup/dao/implementation/UserDaoImpl.class
    #	app/target/shop-1.0-SNAPSHOT/WEB-INF/classes/com/fortegroup/security/controller/AuthenticationController.class
    #	app/target/shop-1.0-SNAPSHOT/WEB-INF/classes/com/fortegroup/security/filter/AuthenticationTokenFilter.class
    #	app/target/shop-1.0-SNAPSHOT/WEB-INF/classes/com/fortegroup/security/model/Message.class
    #	app/target/shop-1.0-SNAPSHOT/WEB-INF/classes/com/fortegroup/security/utill/MessageFactory.class
    #	app/target/shop-1.0-SNAPSHOT/WEB-INF/classes/com/fortegroup/service/UserServiceImpl.class
    #	app/target/shop-1.0-SNAPSHOT/assets/js/app.js
    #	app/target/shop-1.0-SNAPSHOT/assets/js/vendor.js

--- app/src/main/java/com/fortegroup/elasticsearch/controller/ElasticsearchController.java
+++ app/src/main/java/com/fortegroup/elasticsearch/controller/ElasticsearchController.java
@@ -3,10 +3,12 @@ package com.fortegroup.elasticsearch.controller;
 import com.fortegroup.elasticsearch.model.Products;
 import com.fortegroup.elasticsearch.model.SearchRequest;
 import com.fortegroup.elasticsearch.service.ProductsService;
+import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +24,15 @@ public class ElasticsearchController {
     @Autowired
     private ProductsService productsService;
 
-    @RequestMapping("ById")
+    @RequestMapping("category")
     public ResponseEntity<?> getProductsById(@RequestBody SearchRequest request){
-        Page<Products> products = productsService.findById(request.getId(),new PageRequest(0,10));
-        return ResponseEntity.ok(products.getContent());
+        String searchKey = request.getSearchKey();
+
+//        Page<Products> products = productsService(request.getSearchKey(), new PageRequest(0,10));
+
+        return null;/*ResponseEntity.ok(products.getContent());*/
     }
+
+
 
 }
