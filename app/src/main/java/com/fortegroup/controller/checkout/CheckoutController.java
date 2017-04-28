505300b05a023bc7ce18a0107b5e7ffa2c10d95d
Nikita Rybak <mornfrost@rambler.ru>
2017-04-29 01:49:46 +0300

    Add checkout skeleton

---
+++ app/src/main/java/com/fortegroup/controller/checkout/CheckoutController.java
@@ -0,0 +1,14 @@
+package com.fortegroup.controller.checkout;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/rest/checkout")
+public class CheckoutController {
+
+    
+}
