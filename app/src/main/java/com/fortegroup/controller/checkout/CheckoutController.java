66c7228fb969db16dbc0b74ba6e75f4f017dc836
Rybak_Nikita <mornfrost@rambler.ru>
2017-05-04 18:35:30 +0300

    Add PayPal api

--- app/src/main/java/com/fortegroup/controller/checkout/CheckoutController.java
+++ app/src/main/java/com/fortegroup/controller/checkout/CheckoutController.java
@@ -1,18 +1,7 @@
 package com.fortegroup.controller.checkout;
 
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fortegroup.model.checkInformation.ResponseError;
-import com.fortegroup.model.checkInformation.ShippingBilling;
-import com.fortegroup.utill.PropetryFileGenerator;
-import com.litle.sdk.LitleOnline;
-import com.litle.sdk.generate.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.util.Properties;
 
 @RestController
 @RequestMapping(value = "/rest/checkout")
---
+++ app/src/main/java/com/fortegroup/utill/PayPal.java
@@ -0,0 +1,127 @@
+package com.fortegroup.utill;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.util.stream.Collectors;
+
+public class PayPal {
+
+    private static String generateRequestXML(String inputAmount, String cardName, String cardNumber, String cardValidationNumber, String expirationData) throws ParserConfigurationException, TransformerException {
+
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+        Document doc = docBuilder.newDocument();
+        Element litleOnlineRequest = doc.createElement("litleOnlineRequest");
+
+        litleOnlineRequest.setAttribute("merchantId", "01308024");
+        litleOnlineRequest.setAttribute("version", "8.10");
+        litleOnlineRequest.setAttribute("xmlns", "http://www.litle.com/schema");
+
+        doc.appendChild(litleOnlineRequest);
+
+        Element authentication = doc.createElement("authentication");
+        litleOnlineRequest.appendChild(authentication);
+
+        Element user = doc.createElement("user");
+        user.appendChild(doc.createTextNode("u82920470037217304"));
+        authentication.appendChild(user);
+
+        Element lastname = doc.createElement("password");
+        lastname.appendChild(doc.createTextNode("nVCxiyShNoWJaWN"));
+        authentication.appendChild(lastname);
+
+        Element authorization = doc.createElement("authorization");
+        litleOnlineRequest.appendChild(authorization);
+
+        authorization.setAttribute("customerId", "12345");
+        authorization.setAttribute("id", "id");
+        authorization.setAttribute("reportGroup", "rtpGrp");
+
+        Element orderId = doc.createElement("orderId");
+        orderId.appendChild(doc.createTextNode("1"));
+        authorization.appendChild(orderId);
+
+        Element amount = doc.createElement("amount");
+        amount.appendChild(doc.createTextNode(inputAmount));
+        authorization.appendChild(amount);
+
+        Element orderSource = doc.createElement("orderSource");
+        orderSource.appendChild(doc.createTextNode("ecommerce"));
+        authorization.appendChild(orderSource);
+
+        Element card = doc.createElement("card");
+
+        Element type = doc.createElement("type");
+        type.appendChild(doc.createTextNode(cardName));
+        card.appendChild(type);
+
+        Element number = doc.createElement("number");
+        number.appendChild(doc.createTextNode(cardNumber));
+        card.appendChild(number);
+
+        Element expDate = doc.createElement("expDate");
+        expDate.appendChild(doc.createTextNode(expirationData));
+        card.appendChild(expDate);
+
+        Element cardValidationNum = doc.createElement("cardValidationNum");
+        cardValidationNum.appendChild(doc.createTextNode(cardValidationNumber));
+        card.appendChild(cardValidationNum);
+
+        authorization.appendChild(card);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+
+        StringWriter stringWriter = new StringWriter();
+
+        StreamResult result = new StreamResult(new BufferedWriter(stringWriter));
+
+        transformer.transform(source, result);
+
+        return stringWriter.toString();
+
+    }
+
+    private static String getResponseXML(String inputAmount, String cardName, String cardNumber, String cardValidationNumber, String expirationData) throws IOException, TransformerException, ParserConfigurationException {
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpUriRequest request = RequestBuilder.post("https://www.testlitle.com/sandbox/communicator/online")
+                .setEntity(new StringEntity(generateRequestXML(inputAmount, cardName, cardNumber, cardValidationNumber, expirationData))).build();
+        HttpResponse response = httpClient.execute(request);
+
+        InputStream inputStream = response.getEntity().getContent();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String result = bufferedReader.lines().collect(Collectors.joining("\n"));
+
+        return result;
+    }
+
+    public static String getResponseCode(String inputAmount, String cardName, String cardNumber, String cardValidationNumber, String expirationData) throws IOException, TransformerException, ParserConfigurationException {
+        String responseXML = getResponseXML(inputAmount, cardName, cardNumber, cardValidationNumber, expirationData);
+        if(responseXML.contains("message='Error")) {
+            return "bad";
+        }
+        int index = responseXML.indexOf("<response>");
+        return responseXML.substring(index+10, index+13);
+    }
+
+}
