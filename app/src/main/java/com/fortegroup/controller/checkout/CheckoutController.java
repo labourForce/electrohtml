d1691935b9fd14e34097524413d995b0dffb89d4
Alex Burov <iburov64@gmail.com>
2017-05-04 19:49:35 +0300

    Fix merge conflict

--- app/src/main/java/com/fortegroup/controller/checkout/CheckoutController.java
+++ app/src/main/java/com/fortegroup/controller/checkout/CheckoutController.java
@@ -1,14 +1,9 @@
 package com.fortegroup.controller.checkout;
 
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-=======
+
 import org.springframework.web.bind.annotation.*;
->>>>>>> 66c7228fb969db16dbc0b74ba6e75f4f017dc836
+
 
 @RestController
 @RequestMapping(value = "/rest/checkout")
